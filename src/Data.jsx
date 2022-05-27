f46b204521d191e470a633cf1314db3c3ff6bd24
Kiril Komitski <k.komitski94@gmail.com>
2022-05-27 09:05:47 +0100

    ordering fix

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,14 +1,12 @@
-import { collection, onSnapshot, Timestamp } from 'firebase/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { db } from './firebase-config';
 
 export default function Data() {
 	const [data, setData] = useState();
-	// const [manifestos, setManifestos] = useState();
 	const [loading, setLoading] = useState(true);
 
 	const getData = () => {
-		// setLoading(false)
 		onSnapshot(collection(db, 'answers'), (snapshot) => {
 			const snapshotData = snapshot.docs.map((doc) => doc.data());
 			setData(snapshotData);
@@ -17,34 +15,22 @@ export default function Data() {
 	};
 
 	const unixToDate = (timestamp) => {
-		const a = new Date(timestamp * 1000);
-		const months = [
-			'Jan',
-			'Feb',
-			'Mar',
-			'Apr',
-			'May',
-			'Jun',
-			'Jul',
-			'Aug',
-			'Sep',
-			'Oct',
-			'Nov',
-			'Dec',
-		];
-		const year = a.getFullYear();
-		const month = months[a.getMonth()];
-		const date = a.getDate();
-		const hour = a.getHours();
-		const min = a.getMinutes();
-		const sec = a.getSeconds();
-		const time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-		return time;
+		const date = new Date(timestamp);
+		return date.toString().slice(4, -31);
+	};
+
+	const compare = (a, b) => {
+		if (a.createdAt < b.createdAt) {
+			return -1;
+		}
+		if (a.createdAt > b.createdAt) {
+			return 1;
+		}
+		return 0;
 	};
 
 	useEffect(() => {
 		getData();
-		console.log(data);
 	}, []);
 
 	return (
@@ -53,56 +39,59 @@ export default function Data() {
 			<div className='table'>
 				<div className='row'>
 					<div className='cell'>
-						<h4>UID</h4>
+						<h5>UID</h5>
 					</div>
 					<div className='cell'>
-						<h4>Words 1-3</h4>
+						<h5>Words 1-3</h5>
 					</div>
 					<div className='cell'>
-						<h4>Words 4-6</h4>
+						<h5>Words 4-6</h5>
 					</div>
 					<div className='cell'>
-						<h4>Manifesto</h4>
+						<h5>Manifesto</h5>
 					</div>
 					<div className='cell'>
-						<h4>Time</h4>
+						<h5>Time</h5>
 					</div>
 					<div className='cell'>
-						<h4>NFT</h4>
+						<h5>NFT</h5>
 					</div>
 				</div>
 				{loading
 					? 'Loading...'
-					: data.map((point, index) => {
-							return (
-								<div key={index} className='row'>
-									<div className='cell'>
-										<h4>{point.createdAt}</h4>
+					: data
+							.sort(compare)
+							.reverse()
+							.map((point, index) => {
+								return (
+									<div key={index} className='row'>
+										<div className='cell'>
+											<h4>{point.createdAt}</h4>
+										</div>
+										<div className='cell'>
+											<h4>
+												{point.answers[1].A}, {point.answers[2].A}, {point.answers[3].A}
+											</h4>
+										</div>
+										<div className='cell'>
+											<h4>
+												{point.keywords
+													? `${point.keywords[0]}, ${point.keywords[1]}, ${point.keywords[2]}`
+													: '3-4'}
+											</h4>
+										</div>
+										<div className='cell'>
+											<h4>{point.manifesto}</h4>
+										</div>
+										<div className='cell'>
+											<h4>{unixToDate(point.createdAt)}</h4>
+										</div>
+										<div className='cell'>
+											<h4>Yes</h4>
+										</div>
 									</div>
-									<div className='cell'>
-										<h4>
-											{point.answers[1].A}, {point.answers[2].A}, {point.answers[3].A}
-										</h4>
-									</div>
-									<div className='cell'>
-										<h4>
-											{point.keywords
-												? `${point.keywords[0]}, ${point.keywords[1]}, ${point.keywords[2]}`
-												: '3-4'}
-										</h4>
-									</div>
-									<div className='cell'>
-										<h4>{point.manifesto}</h4>
-									</div>
-									<div className='cell'>
-										<h4>{unixToDate(point.createdAt)}</h4>
-									</div>
-									<div className='cell'>
-										<h4>Yes</h4>
-									</div>
-								</div>
-							);
-					  })}
+								);
+							})}
 			</div>
 		</>
 	);
